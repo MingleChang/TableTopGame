973dc18f1e535eff545c21772fb291433566398f
Mingle Chang <mingle890805@gmail.com>
2021-08-29 15:52:23 +0800

    create

---
+++ Client/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Client
+//
+//  Created by MingleChang on 2021/8/29.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
---
+++ Server/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  Server
+//
+//  Created by MingleChang on 2021/8/29.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
